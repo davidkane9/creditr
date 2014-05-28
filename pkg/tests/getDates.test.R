2239a852726211ce33f5ebbf0d76f6f1ea60ca0e
kanishkamalik <kanishka.college@gmail.com>
2014-05-28 14:39:37 -0400

    made a small change to getDates.test

--- pkg/tests/getDates.test.R
+++ pkg/tests/getDates.test.R
@@ -2,4 +2,12 @@
 
 library(CDS)
 ## different dates for a CDS with a 10-year maturity
-getDates(TDate = as.Date("2014-04-22"), maturity="10Y")
+truth1 <- data.frame(TDate = as.Date("2014-04-15"), stepinDate = as.Date("2014-04-16"), 
+           valueDate = as.Date("2014-04-18"), startDate = as.Date("2014-03-20"), 
+           firstcouponDate = as.Date("2014-06-20"),
+           pencouponDate=as.Date("2019-03-20"), endDate = "2019-06-20", 
+           backstopDate = as.Date("2014-02-14"))
+result1 <- getDates(TDate = as.Date("2014-04-15"), maturity="5Y")
+stopifnot(all.equal(truth1, result1))
+
+expect_that(result1, is_identical_to(truth1))
