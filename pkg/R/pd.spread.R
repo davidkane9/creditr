c108f7d393a35f170f43ae372a79274aafc9671d
kanishkamalik <kanishka.college@gmail.com>
2014-07-21 10:02:29 -0400

    pd.spread

--- pkg/R/pd.spread.R
+++ pkg/R/pd.spread.R
@@ -3,7 +3,8 @@
 #' @param R recovery rate in decimals
 #' @param PD probability of default in decimals
 #' @param time in years
-#' @return spread in basis points
+#' @return spread in basis points, calculated by inverting the formula for probability
+#' of default given in the Bloomberg Manual
 
 
 pd.spread <- function(PD, time, Recovery = 0.4){
