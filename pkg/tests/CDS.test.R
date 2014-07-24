cda0b9836a2af497f46119b8f400f7c1a89b45cb
kanishkamalik <kanishka.college@gmail.com>
2014-07-24 17:51:46 -0400

    Minor change to last section of vignette.

--- pkg/tests/CDS.test.R
+++ pkg/tests/CDS.test.R
@@ -1,17 +1,19 @@
 ## CDS.R test case
 
 library(CDS)
-## truth1 <- CDS(TDate = "2014-01-14",
-##               tenor = "5Y",
-##               parSpread = 32,
-##               coupon = 100,
-##               recoveryRate = 0.4,
-##               isPriceClean = FALSE,
-##               notional = 1e7)
+
+truth1 <- CDS(TDate = "2014-01-14",
+               tenor = "5Y",
+               parSpread = 32,
+               coupon = 100,
+               recoveryRate = 0.4,
+               isPriceClean = FALSE,
+               notional = 1e7)
 
 ## save(truth1, file = "CDS.test.RData")
 
-load("CDS.test.RData")
+## load("CDS.test.RData")
+
 result1 <- CDS(TDate = "2014-01-14",
                tenor = "5Y",
                parSpread = 32,
