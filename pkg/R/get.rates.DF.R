711bd5145fd3aac10a139e3a6fb0bf241ba6f251
David Kane <dave.kane@gmail.com>
2014-08-07 10:17:45 -0400

    cleaning up some horrible code

--- pkg/R/get.rates.DF.R
+++ pkg/R/get.rates.DF.R
@@ -1,33 +1,33 @@
-#' \code{} takes a data frame of dates and returns a data frame with the 
-#' yields for different maturities. Note year must be entered as YYYY-MM-DD.
+#' \code{get.rates.DF} takes a data frame of dates and returns a data frame with
+#' the yields for different maturities. Note year must be entered as YYYY-MM-DD.
 #' 
-#' @param startDate date from when rate calculation starts
-#' @param endDate date when rate calculation ends
-#' @param currency for which rates are being calculated
-#' @return dataframe containing the rates from everyday between and including
-#' the start and end dates. Note: the date in the column does not refer to the
-#' rates of that day but the date on which the CDS is being priced. So the 
-#' corresponding rate is actually the rate of the previous day. Example: if the 
-#' column reads 2014-04-22, the corresponding rates are actually for 2014-04-21.
-#' 
-get.rates.DF <- function(startDate, endDate, currency="USD"){
+#' @param start date for gathering interest rates
+#' @param end date for gathering interest rates
+#' @param currency for which rates are being retrieved
+#' @return data frame containing the rates from every day from start to end 
+#'   dates. Note: the date in the output data frame does not refer to the rates 
+#'   of that day but to the date on which the CDS is being priced. So the 
+#'   corresponding rate is actually the rate of the previous day. Example: if 
+#'   the column reads 2014-04-22, the corresponding rates are actually for 
+#'   2014-04-21.
+ 
+get.rates.DF <- function(start, end, currency = "USD"){
+
   yearRates <- NULL
   endDate <- as.Date(endDate)
   startDate <- as.Date(startDate)
   
   # start & end date must be valid
   
-  expect_that(checkDate(startDate), prints_text("NA"))
-  expect_that(checkDate(endDate), prints_text("NA"))
-  
+  stopifnot(check.date(start))
+  stopifnot(check.date(end))
+    
   # start date must be smaller than end date
   
-  expect_that(endDate>startDate, prints_text("TRUE"))
-  
-  # year = 1900 + as.POSIXlt(date)$year 
+  stopifnot(endDate <= startDate)
   
   while(endDate>startDate){
-    Rates <- try(getRates(date = endDate, currency=currency)[[1]])
+    Rates <- try(get.rates(date = endDate, currency=currency)[[1]])
     if(is(Rates, "try-error")){
       getRates <- NULL
       endDate <- endDate - 1 
@@ -40,12 +40,8 @@ get.rates.DF <- function(startDate, endDate, currency="USD"){
       df <- data.frame(date, currency, expiry, rates)
       yearRates <- rbind(yearRates, df)
       endDate <- endDate - 1      
-    }
-    
-    # year = 1900 + as.POSIXlt(date)$year
-    
+    }    
   }  
+  
   return(yearRates)
 }
-
-
