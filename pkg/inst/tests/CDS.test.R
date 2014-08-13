c639f1d861a487adf8d2bad76388945297cb46aa
kanishkamalik <kanishka.college@gmail.com>
2014-08-13 14:06:50 -0400

    Moved sources directory

--- pkg/inst/tests/CDS.test.R
+++ pkg/inst/tests/CDS.test.R
@@ -1,5 +1,5 @@
 ## CDS.R test case for Caesars Entertainment Operating Co Inc using data obtainined from Bloomberg.
-## The .png files containing these results are in tests/images
+## The .png files containing these results are in tests/sources
 
 library(CDS)
 
