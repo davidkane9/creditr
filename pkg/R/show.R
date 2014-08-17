2c50c17966988ca7c82a9db2cc1053583b7f288e
knightsay <knightsay@hotmail.com>
2014-08-17 15:38:05 -0400

    replace .check.length in show.R

--- pkg/R/show.R
+++ pkg/R/show.R
@@ -18,58 +18,58 @@ setMethod("show",
 
               cat(sprintf(paste("Contract Type:", object@contract,
                                 sep = paste0(rep(" ",
-                                    40-.check.length("Contract Type:") -
-                                    .check.length(object@contract)), collapse = ""))),
+                                    40-nchar(as.character("Contract Type:")) -
+                                    nchar(as.character(object@contract))), collapse = ""))),
                   sprintf(paste("   Currency:", object@currency,
                                 sep = paste0(rep(" ",
-                                    40-.check.length("   Currency:") -
-                                    .check.length(object@currency)),
+                                    40-nchar(as.character("   Currency:")) -
+                                    nchar(as.character(object@currency))),
                                     collapse = ""))), "\n",
 
                   sprintf(paste("Entity Name:", object@entityName,
                                 sep = paste0(rep(" ",
-                                    40-.check.length("Entity Name:") -
-                                    .check.length(object@entityName)), collapse = ""))),
+                                    40-nchar(as.character("Entity Name:")) -
+                                    nchar(as.character(object@entityName))), collapse = ""))),
                   sprintf(paste("   RED:", object@RED,
                                 sep = paste0(rep(" ",
-                                    40-.check.length("   RED:") -
-                                    .check.length(object@RED)),
+                                    40-nchar(as.character("   RED:")) -
+                                    nchar(as.character(object@RED))),
                                     collapse = ""))), "\n",
 
                   sprintf(paste("TDate:", object@TDate,
-                                sep = paste0(rep(" ", 40-.check.length("TDate:") -
-                                    .check.length(object@TDate)), collapse = ""))),
+                                sep = paste0(rep(" ", 40-nchar(as.character("TDate:")) -
+                                    nchar(as.character(object@TDate))), collapse = ""))),
                   sprintf(paste("   End Date:", object@endDate,
-                                sep = paste0(rep(" ", 40-.check.length("   End Date:") -
-                                    .check.length(object@endDate)),
+                                sep = paste0(rep(" ", 40-nchar(as.character("   End Date:")) -
+                                    nchar(as.character(object@endDate))),
                                     collapse = ""))), "\n",
 
                   sprintf(paste("Start Date:", object@startDate,
-                                sep = paste0(rep(" ", 40-.check.length("Start Date:") -
-                                    .check.length(object@startDate)), collapse = ""))),
+                                sep = paste0(rep(" ", 40-nchar(as.character("Start Date:")) -
+                                    nchar(as.character(object@startDate))), collapse = ""))),
                   sprintf(paste("   Backstop Date:", object@backstopDate,
                                 sep = paste0(rep(" ", 40-
-                                    .check.length("   Backstop Date:") -
-                                    .check.length(object@backstopDate)),
+                                    nchar(as.character("   Backstop Date:")) -
+                                    nchar(as.character(object@backstopDate))),
                                     collapse = ""))), "\n",
 
                   sprintf(paste("1st Coupon:", object@firstcouponDate,
-                                sep = paste0(rep(" ", 40-.check.length("1st Coupon:") -
-                                    .check.length(object@firstcouponDate)),
+                                sep = paste0(rep(" ", 40-nchar(as.character("1st Coupon:")) -
+                                    nchar(as.character(object@firstcouponDate))),
                                     collapse = ""))),
                   sprintf(paste("   Pen Coupon:", object@pencouponDate,
                                 sep = paste0(rep(" ", 40-
-                                    .check.length("   Pen Coupon:") -
-                                    .check.length(object@pencouponDate)),
+                                    nchar(as.character("   Pen Coupon:")) -
+                                    nchar(as.character(object@pencouponDate))),
                                     collapse = ""))), "\n",
 
                   sprintf(paste("Day Cnt:", object@convention['dccCDS'],
                                 sep = paste0(rep(" ",
-                                    40-.check.length("Day Cnt:") -
-                                    .check.length(object@convention['dccCDS'])), collapse = ""))),
+                                    40-nchar(as.character("Day Cnt:")) -
+                                    nchar(as.character(object@convention['dccCDS']))), collapse = ""))),
                   sprintf(paste("   Freq:", object@convention['freqCDS'],
-                                sep = paste0(rep(" ", 40-.check.length("   Freq:") -
-                                    .check.length(object@convention['freqCDS'])),
+                                sep = paste0(rep(" ", 40-nchar(as.character("   Freq:")) -
+                                    nchar(as.character(object@convention['freqCDS']))),
                                     collapse = ""))), "\n",
                   sep = ""
                   )
@@ -79,101 +79,101 @@ setMethod("show",
               
               cat(sprintf(paste("Value Date:", object@valueDate,
                                 sep = paste0(rep(" ",
-                                    40-.check.length("Value Date:") -
-                                    .check.length(object@valueDate)),
+                                    40-nchar(as.character("Value Date:")) -
+                                    nchar(as.character(object@valueDate))),
                                     collapse = ""))),
                   sprintf(paste("   Price:",
                                 format(round(object@price, 2), big.mark=",",
                                        scientific = F),
-                                sep = paste0(rep(" ", 40-.check.length("   Price:") -
-                                    .check.length(
+                                sep = paste0(rep(" ", 40-nchar(as.character("   Price:")) -
+                                    nchar(as.character(
                                         format(round(object@price, 2), big.mark=",",
-                                               scientific=F))),
+                                               scientific=F)))),
                                     collapse = ""))), "\n",
                   
                   sprintf(paste("Spread:",
                                 format(round(object@parSpread, 4), big.mark = ",",
                                        scientific = F),
                                 sep = paste0(rep(" ",
-                                    40-.check.length("Spread:") -
-                                    .check.length(format(round(object@parSpread, 4), big.mark = ",",
-                                                        scientific = F))),
+                                    40-nchar(as.character("Spread:")) -
+                                    nchar(as.character(format(round(object@parSpread, 4), big.mark = ",",
+                                                        scientific = F)))),
                                     collapse = ""))),
                   sprintf(paste("   Pts Upfront:",
                                 format(round(object@ptsUpfront, 4), big.mark=",",
                                        scientific=F),
-                                sep = paste0(rep(" ", 40-.check.length("   Pts Upfront:") -
-                                    .check.length(
+                                sep = paste0(rep(" ", 40-nchar(as.character("   Pts Upfront:")) -
+                                    nchar(as.character(
                                         format(round(object@ptsUpfront, 4), big.mark=",",
-                                               scientific=F))),
+                                               scientific=F)))),
                                     collapse = ""))), "\n",
 
                   sprintf(paste("Principal:",
                                 format(round(object@principal, 0), big.mark=",",
                                        scientific=F),
                                 sep = paste0(rep(" ",
-                                    40-.check.length("Principal:") -
-                                    .check.length(
-                                        format(round(object@principal, 0), big.mark = "F", scientific = F))),
+                                    40-nchar(as.character("Principal:")) -
+                                    nchar(as.character(
+                                        format(round(object@principal, 0), big.mark = "F", scientific = F)))),
                                     collapse = ""))),
                   sprintf(paste("   Spread DV01:",
                                 format(round(object@spreadDV01, 0), big.mark=",",
                                        scientific=F),
-                                sep = paste0(rep(" ", 40-.check.length("   Spread DV01:") -
-                                    .check.length(
+                                sep = paste0(rep(" ", 40-nchar(as.character("   Spread DV01:")) -
+                                    nchar(as.character(
                                         format(round(object@spreadDV01, 0), big.mark=",",
-                                               scientific=F))),
+                                               scientific=F)))),
                                     collapse = ""))), "\n",
                   
                   sprintf(paste("Accrual:",
                                 format(round(object@accrual, 0), big.mark=",",
                                        scientific=F),
                                 sep = paste0(rep(" ",
-                                    40-.check.length("Accrual:") -
-                                    .check.length(
+                                    40-nchar(as.character("Accrual:")) -
+                                    nchar(as.character(
                                         format(round(object@accrual, 0), big.mark = ",",
-                                               scientific=F))),
+                                               scientific=F)))),
                                     collapse = ""))), 
                   sprintf(paste("   IR DV01:", format(round(object@IRDV01, 2),big.mark=",",
                                                       scientific=F),
                                 sep = paste0(rep(" ",
-                                    40-.check.length("   IR DV01:") -
-                                    .check.length(
+                                    40-nchar(as.character("   IR DV01:")) -
+                                    nchar(as.character(
                                         format(round(object@IRDV01, 2), big.mark=",",
-                                               scientific=F))),
+                                               scientific=F)))),
                                     collapse = ""))), "\n",
 
                   sprintf(paste("Upfront:",
                                 format(round(object@upfront, 0), big.mark = ",",
                                        scientific=F),
                                 sep = paste0(rep(" ",
-                                    40-.check.length("Upfront:") -
-                                    .check.length(
+                                    40-nchar(as.character("Upfront:")) -
+                                    nchar(as.character(
                                         format(round(object@upfront, 0), big.mark=",",
-                                               scientific=F))),
+                                               scientific=F)))),
                                     collapse = ""))), 
                   sprintf(paste("   Rec Risk (1 pct):",
                                 format(round(object@RecRisk01, 2),big.mark=",",
                                        scientific=F),
-                                sep = paste0(rep(" ", 40-.check.length("   Rec Risk (1 pct):") -
-                                    .check.length(
+                                sep = paste0(rep(" ", 40-nchar(as.character("   Rec Risk (1 pct):")) -
+                                    nchar(as.character(
                                         format(round(object@RecRisk01, 2),
-                                               big.mark=",", scientific=F))),
+                                               big.mark=",", scientific=F)))),
                                     collapse = ""))), "\n",
 
                   sprintf(paste("Default Prob:", round(object@defaultProb, 4),
                                 sep = paste0(rep(" ",
-                                    40-.check.length("Default Prob:") -
-                                    .check.length(round(object@defaultProb, 4))),
+                                    40-nchar(as.character("Default Prob:")) -
+                                    nchar(as.character(round(object@defaultProb, 4)))),
                                     collapse = ""))), 
                   sprintf(paste("   Default Expo:",
                                 format(round(object@defaultExpo, 0),big.mark=",",
                                        scientific=F),
-                                sep = paste0(rep(" ", 40-.check.length("   Default Expo:") -
-                                    .check.length(
+                                sep = paste0(rep(" ", 40-nchar(as.character("   Default Expo:")) -
+                                    nchar(as.character(
                                         format(round(object@defaultExpo, 0),
                                                big.mark=",",
-                                               scientific=F))),
+                                               scientific=F)))),
                                     collapse = ""))), "\n",
 
                   sep = ""
