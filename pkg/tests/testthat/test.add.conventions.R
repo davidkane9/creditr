84135f7d5737c39e1a3697ec0bce7064dcf7cbca
Yuanchu <Yuanchu.Dang@williams.edu>
2014-08-20 09:36:18 -0400

    test case for add.conventions

---
+++ pkg/tests/testthat/test.add.conventions.R
@@ -0,0 +1,23 @@
+context("Test add.conventions")
+
+test_that("test add conventions", {
+  
+  ## used independently
+  
+  x1 <- data.frame(date = c(as.Date("2014-05-06"), as.Date("2014-05-07")), currency = c("USD", "JPY"))
+  result1 <- add.conventions(x2)
+  
+  x2 <- data.frame(date = c(as.Date("2014-04-22"), as.Date("2014-04-22")),
+                  currency = c("USD", "EUR"),
+                  tenor = c(5, 5),
+                  spread = c(120, 110),
+                  coupon = c(100, 100),
+                  recovery.rate = c(0.4, 0.4),
+                  notional = c(1e7, 1e7))
+  
+  ## joint usage with add.dates
+  
+  x2 <- add.dates(x2)
+  result2 <- add.conventions(x2)
+  
+})
