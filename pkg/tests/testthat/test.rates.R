3cf345dcacd5e3a33aa2b19a87c24b36c9adcee7
David Kane <dave.kane@gmail.com>
2014-08-21 07:57:30 -0400

    initial tests for rates

---
+++ pkg/tests/testthat/test.rates.R
@@ -0,0 +1,22 @@
+context("Test values in rates data frame")
+
+data(rates)
+
+test_that("test that rates data frame has correct variable names and types", {
+  
+  expect_true(all.equal(names(rates), c("date", "currency", "expiry", "rates")))
+    
+
+})
+
+test_that("test that rates data frame variables currency and expiry have only allowed values", {
+  
+  expect_true(all(rates$currency %in% c("USD", "JPY", "EUR", "GBP")))
+  
+  expect_true(all(rates$expiry %in% c("1M", "2M", "3M", "6M", "9M",
+                                      "1Y", "2Y", "3Y", "4Y", "5Y",  
+                                      "6Y", "7Y", "8Y", "9Y",
+                                      "10Y", "12Y", "15Y", "20Y", "25Y", "30Y")))
+  
+  
+})
