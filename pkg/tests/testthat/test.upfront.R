795e97b35b74d16ac887052a612825892e67c441
Yuanchu <Yuanchu.Dang@williams.edu>
2014-08-22 11:32:44 -0400

    revise upfront test case

--- pkg/tests/testthat/test.upfront.R
+++ pkg/tests/testthat/test.upfront.R
@@ -293,19 +293,19 @@ test_that("2nd set of test for upfront", {
   ## test case for Norske Skogindustrier and Chorus Ltd. Create a data frame with 
   ## information from these two CDSs repeated 5000 times each.
   
-  x.1 <- data.frame(date = c("2014-04-15", "2014-04-22"),  
-                    tenor = c(5, 5), 
-                    coupon = c(500, 100), 
-                    spread = c(2785.8889, 99),
-                    currency = c("EUR", "EUR"),
-                    recovery = c(0.4, 0.4))
+  x.1 <- data.frame(date = c(rep("2014-04-15", 100), rep("2014-04-22", 100)),  
+                    tenor = c(rep(5, 100), rep(5, 100)), 
+                    coupon = c(rep(500, 100), rep(100, 100)), 
+                    spread = c(rep(2785.8889, 100), rep(99, 100)),
+                    currency = c(rep("EUR", 100), rep("EUR", 100)),
+                    recovery = c(rep(0.4, 100), rep(0.4, 100)))
   
-  x.2 <- data.frame(date = c("2014-04-15", "2014-04-22"),  
-                    maturity = c("2019-06-20", "2019-06-20"), 
-                    coupon = c(500, 100), 
-                    spread = c(2785.8889, 99),
-                    currency = c("EUR", "EUR"),
-                    recovery = c(0.4, 0.4))
+  x.2 <- data.frame(date = c(rep("2014-04-15", 100), rep("2014-04-22", 100)),  
+                    maturity = c(rep("2019-06-20", 100), rep("2019-06-20", 100)), 
+                    coupon = c(rep(500, 100), rep(100, 100)), 
+                    spread = c(rep(2785.8889, 100), rep(99, 100)),
+                    currency = c(rep("EUR", 100), rep("EUR", 100)),
+                    recovery = c(rep(0.4, 100), rep(0.4, 100)))
   
   ##  save(x.1, x.2, file = "upfrontdf.test.RData")
   
@@ -315,8 +315,8 @@ test_that("2nd set of test for upfront", {
   ## which required the results to be rounded off to the nearest whole number
   
   result.1 <- upfront(x = x.1, rates = rates, tenor.var = "tenor")
-  expect_that(round(result.1, -2), equals(c(4412500, round(-14368, -2))))
+  expect_that(round(result.1, -2), equals(c(rep(4412500, 100), rep(round(-14368, -2), 100))))
   
   result.2 <- upfront(x.2, rates, tenor = "tenor")
-  expect_that(round(result.2, -2), equals(c(4412500, round(-14368, -2))))
+  expect_that(round(result.2, -2), equals(c(rep(4412500, 100), rep(round(-14368, -2), 100))))
 })
