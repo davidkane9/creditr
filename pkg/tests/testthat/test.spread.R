827510bc1c6bd0b2453194663178b814c6263e50
Yuanchu <Yuanchu.Dang@williams.edu>
2014-08-24 00:49:02 -0400

    attempt to update spread test case

--- pkg/tests/testthat/test.spread.R
+++ pkg/tests/testthat/test.spread.R
@@ -8,43 +8,20 @@ test_that("test for spread", {
   
   load("spread.test.RData")
   
-  result.1 <- spread(date = as.Date("2014-04-22"),
-                     currency = "USD",
-                     tenor = 5,
-                     types = "MMMMMSSSSSSSSSSSSSS",
-                     rates = c(1.522e-3, 1.929e-3, 2.259e-3, 3.198e-3, 5.465e-3, 
-                               5.380e-3, 1.000e-2, 1.4475e-2, 1.8165e-2, 2.1180e-2, 
-                               2.3490e-2, 2.5430e-2, 2.6955e-2, 2.8250e-2, 3.0275e-2,
-                               3.2285e-2, 3.3970e-2, 3.4730e-2, 3.5070e-2),
-                     expiries = c("1M", "2M", "3M", "6M", "12M", "2Y", "3Y", 
-                                  "4Y", "5Y", "6Y", "7Y", "8Y", "9Y", "10Y", "12Y",
-                                  "15Y", "20Y", "25Y", "30Y"),                  
-                     
-                     valueDate = "2014-04-25",
-                     benchmarkDate = "2013-12-20",
-                     startDate = "2014-03-20",
-                     endDate = "2019-06-20",
-                     stepinDate = "2014-04-23",
-     
-                     upfront = 18624,
-                     coupon = 100, 
-                     recovery.rate = .4,
-                     payAccruedAtStart = FALSE,
+  x1 <- data.frame(date = as.Date("2014-04-22"),
+                   currency = "USD",
+                   coupon = 100,
+                   recovery.rate = .4,
+                   tenor = 5,
+                   upfront = 18624)
+  
+  result.1 <- spread(x = x1,
+                     isPriceClean = FALSE,
                      notional = 1e7,
-                     payAccruedOnDefault = TRUE,
-                   
-                     mmDCC = "Act/360",
-                     fixedSwapFreq = "6M",
-                     floatSwapFreq = "6M",
-                     fixedSwapDCC = "30/360",
-                     floatSwapDCC = "30/360",
-                     badDayConvZC = 'M',
-                     holidays = 'None',
-                     dccCDS = "ACT/360",
-                     freqCDS = 'Q',      
-                     stubCDS = "F", 		
-                     badDayConvCDS = "F",
-                     calendar = 'None')
+                     freqCDS = "Q",
+                     stubCDS = "F",
+                     payAccruedAtStart = FALSE,
+                     payAccruedOnDefault = TRUE)
   
   ## test passes when results are rounded off to the second decimal place
   
