424a1e53fdd9e0d0f3cdeddc6536ded98688525d
davidkane9 <dave.kane@gmail.com>
2014-08-25 22:55:36 -0400

    clean up some documentation

--- pkg/R/implied.RR.R
+++ pkg/R/implied.RR.R
@@ -1,32 +1,28 @@
-#' Calculates RR
+#' Calculates Implied Recovery Rate
 #' 
-#' \code{implied.RR} that calculates the implied recovery rate.
+#' \code{implied.RR} that calculates the recovery rate implied by the CDS spread
+#' and probability of default (pd) by using the ISDA model. This takes a data 
+#' frame of inputs and returns a vector of the same length.
 #' 
-#' Function that uses the ISDA model to calculate the implied recovery
-#' rate for cases in which we have a spread and a probability of
-#' default (pd).  This takes a data frame of inputs and returns a
-#' vector of the same length. If we have a data frame like:
-#' id,spread,pd which we pass into a function, this then returns a
-#' vector of implied default rates. 
-#'
-#' @param data dataframe containing the 1. probability of default (pd) over a 
-#' a certain tenor period, 2. id and 3. spread
+#' @param data dataframe containing the 1. probability of default (pd) over a a 
+#'   certain tenor period, 2. id and 3. spread
 #' @param spread.var name of the column containing the spread
-#' @param end.date.var name of the column containing the end dates or maturity dates. 
-#' This is when the contract expires and protection ends. Any default after 
-#' this date does not trigger a payment.
+#' @param end.date.var name of the column containing the end dates or maturity 
+#'   dates. This is when the contract expires and protection ends. Any default 
+#'   after this date does not trigger a payment.
 #' @param date.var name of the column containing the trade dates.
 #' @param pd.var name of the column containing the probability of default rates.
-#' 
-#' @return implied recovery rate in percentage based on the general approximation 
-#' for a probability of default in the Bloomberg manual. The actual calculation uses 
-#' a complicated bootstrapping process, so the results may be marginally different.
+#'   
+#' @return implied recovery rate in percentage based on the general 
+#'   approximation for a probability of default in the Bloomberg manual. The 
+#'   actual calculation uses a complicated bootstrapping process, so the results
+#'   may be marginally different.
 
 implied.RR <- function(data, 
-                       spread.var = "spread", 
-                       pd.var = "pd", 
+                       spread.var   = "spread", 
+                       pd.var       = "pd", 
                        end.date.var = "end.date", 
-                       date.var = "date"){
+                       date.var     = "date"){
   
   ## stop if the required columns are not contained in the data frame
   
