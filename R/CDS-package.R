1df4d5b04d3a2d81eb9272c35cd004688978810c
Miller-Zijie-Zhu <zijie.zhu@hutchinhill.com>
2015-07-28 14:45:09 -0400

    doc roxygen

---
+++ R/CDS-package.R
@@ -0,0 +1,20 @@
+#' The CDS package.
+#'
+#' 
+#'
+#' @name CDS
+#' @docType package
+#' 
+#' @useDynLib
+#' @exportPattern "^[[:alpha:]]+"
+#'
+#' @exportClass CDS
+#' @exportMethod summary show
+#' 
+#' @import methods
+#' @import Rcpp
+#' @import RCurl
+#' @import XML
+#' @import testthat
+#' @import xts
+NULL
