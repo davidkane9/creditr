11f12b55772e9eda378767ed3cbeda5500d58466
davidkane9 <dave.kane@gmail.com>
2015-08-09 07:26:53 -0400

    Don't think we need this anymore because we don't have two packages now.

--- R/zzz.R
+++ R/zzz.R
@@ -4,8 +4,4 @@
   ## The below works and please don't delete this!
   
   Sys.setenv("_R_BUILD_COMPACT_VIGNETTES_" = "gs+qpdf")
-  
-  ## do not check suggests package (creditrISDA) dependency 
-  
-  Sys.setenv("_R_CHECK_FORCE_SUGGESTS_" = FALSE)
 }
