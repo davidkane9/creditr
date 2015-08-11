4c971e84e686edf8568983cde197fb43f82a1863
Zijie Zhu (Miller) <knightsay@hotmail.com>
2015-08-11 13:08:50 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
   ## compress the vignette PDF to fix CMD Check WARNING
   ## The below works and please don't delete this!
   
-  Sys.setenv("_R_BUILD_COMPACT_VIGNETTES_" = "gs+qpdf")
+  Sys.setenv("_R_BUILD_COMPACT_VIGNETTES_" = "qpdf")
 }
 
 .onAttach <- function(...) {
