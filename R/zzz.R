ff5b49fcf3e6398c225cae03fd375349a690e49f
davidkane9 <dave.kane@gmail.com>
2015-08-15 10:40:08 -0400

    clean up message for on Attach

--- R/zzz.R
+++ R/zzz.R
@@ -8,24 +8,19 @@
 
 .onAttach <- function(...) {
   packageStartupMessage(
-    "\nIF YOU USE THIS CDS PACKAGE, YOUR USE WILL SIGNIFY YOUR
-    UNDERSTANDING \nAND IRREVOCABLE ACCEPTANCE OF THIS LICENSE AND ITS
-    TERMS, WHICH INCORPORATE \nBY REFERENCE THE INTERNATIONAL SWAPS AND
-    DERIVATIVES ASSOCIATION, INC.'S CDS \nSTANDARD MODEL PUBLIC LICENSE,
+    "\nIF YOU USE THIS creditr PACKAGE, YOUR USE WILL SIGNIFY YOUR
+    UNDERSTANDING AND IRREVOCABLE ACCEPTANCE OF THIS LICENSE AND ITS
+    TERMS, WHICH INCORPORATE BY REFERENCE THE INTERNATIONAL SWAPS AND
+    DERIVATIVES ASSOCIATION, INC.'S CDS STANDARD MODEL PUBLIC LICENSE,
     WHICH IS AVAILABLE AT
-    \nhttp://www.cdsmodel.com/cdsmodel/cds-disclaimer.page before using
-    the package. \n\nNOTHING IN THIS LICENSE RESTRICTS YOUR ABILITY TO USE
-    THE ISDA(R) CDS \nSTANDARD MODEL.\n \nDISCLAIMER: ISDA HAS NEITHER
-    REVIEWED, APPROVED NOR ENDORSED THE USE OF \nTHE CDS PACKAGE. THOSE
-    PERSONS USING THIS CDS PACKAGE ARE ENCOURAGED TO SEEK \nTHE ADVICE OF
-    A LEGAL PROFESSIONAL TO EVALUATE ITS SUITABILITY FOR THEIR USE.\n
+    http://www.cdsmodel.com/cdsmodel/cds-disclaimer.page 
+    before using the package.
+    \nNOTHING IN THIS LICENSE RESTRICTS YOUR ABILITY TO USE
+    THE ISDA(R) CDS STANDARD MODEL.
+    \nDISCLAIMER: ISDA HAS NEITHER REVIEWED, APPROVED NOR ENDORSED THE 
+    USE OF THE creditr PACKAGE. THOSE PERSONS USING THIS CDS PACKAGE 
+    ARE ENCOURAGED TO SEEK THE ADVICE OF A LEGAL PROFESSIONAL TO EVALUATE 
+    ITS SUITABILITY FOR THEIR USE.
     \nISDA(R) is a registered mark of the International Swaps and
-    Derivatives \nAssociation, Inc.\n \nPlease type **yes** to assent to
-    the aforementioned terms.\n")
-  
-  if(interactive()){
-    while (readLines(n=1)!="yes")
-      packageStartupMessage("Please type **yes** to assent to the
-                             aforementioned terms.\n")
-  }
+    Derivatives Association, Inc.")
 }
